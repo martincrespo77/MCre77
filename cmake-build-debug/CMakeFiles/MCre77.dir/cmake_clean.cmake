643dac91d8e35c7d5d727f541bb8edfe9fa2a5c5
martincrespo77 <mcrespo@fie.undef.edu.ar>
2023-03-28 10:18:26 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/MCre77.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MCre77.dir/main.cpp.o"
+  "CMakeFiles/MCre77.dir/main.cpp.o.d"
+  "MCre77"
+  "MCre77.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MCre77.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MCre77.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MCre77.
+# This may be replaced when dependencies are built.
